7e47585365f95d5d9ee790c01e96864db8f2ea19
rrorim <or.imon@tuni.fi>
2023-01-21 02:04:25 +0200

    Countries REST Api

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>,
-  document.getElementById("root")
+ReactDOM.render( <
+    React.StrictMode >
+    <
+    Router >
+    <
+    App / >
+    <
+    /Router> <
+    /React.StrictMode>,
+    document.getElementById("root")
 );
